8c444fb5ba32c5e25da70fd30e1a1b4b8f8f9fe9
Paul Anderson <paul.robert.anderson@gmail.com>
2016-01-03 18:57:12 +1100

    Initial commit

---
+++ WordFinder/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WordFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlLetters = new System.Windows.Forms.Panel();
+            this.lettersGrid = new System.Windows.Forms.TableLayoutPanel();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.lstResults = new System.Windows.Forms.ListBox();
+            this.pnlLetters.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(13, 13);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(175, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load Dictionary";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Letters";
+            // 
+            // pnlLetters
+            // 
+            this.pnlLetters.Controls.Add(this.lettersGrid);
+            this.pnlLetters.Location = new System.Drawing.Point(13, 71);
+            this.pnlLetters.Name = "pnlLetters";
+            this.pnlLetters.Size = new System.Drawing.Size(250, 180);
+            this.pnlLetters.TabIndex = 2;
+            // 
+            // lettersGrid
+            // 
+            this.lettersGrid.BackColor = System.Drawing.Color.White;
+            this.lettersGrid.ColumnCount = 4;
+            this.lettersGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.lettersGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.lettersGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.lettersGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.lettersGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lettersGrid.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lettersGrid.Location = new System.Drawing.Point(0, 0);
+            this.lettersGrid.Name = "lettersGrid";
+            this.lettersGrid.RowCount = 4;
+            this.lettersGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.lettersGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.lettersGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.lettersGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.lettersGrid.Size = new System.Drawing.Size(250, 180);
+            this.lettersGrid.TabIndex = 3;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(188, 257);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 3;
+            this.btnFind.Text = "Find ->";
+            this.btnFind.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(13, 257);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear X";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // lblResults
+            // 
+            this.lblResults.AutoSize = true;
+            this.lblResults.Location = new System.Drawing.Point(288, 55);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(42, 13);
+            this.lblResults.TabIndex = 1;
+            this.lblResults.Text = "Results";
+            // 
+            // lstResults
+            // 
+            this.lstResults.FormattingEnabled = true;
+            this.lstResults.Location = new System.Drawing.Point(291, 71);
+            this.lstResults.Name = "lstResults";
+            this.lstResults.Size = new System.Drawing.Size(401, 212);
+            this.lstResults.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 292);
+            this.Controls.Add(this.lstResults);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.pnlLetters);
+            this.Controls.Add(this.lblResults);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
+            this.Text = "Word Finder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlLetters.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlLetters;
+        private System.Windows.Forms.TableLayoutPanel lettersGrid;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblResults;
+        private System.Windows.Forms.ListBox lstResults;
+    }
+}
+
---
+++ WordFinder/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordFinder
+{
+
+    public partial class Form1 : Form
+    {
+        const int gridSize = 4;
+        private char[,] letters;
+        private TextBox[,] letterControls;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            letters = new char[gridSize, gridSize];
+            letterControls = new TextBox[gridSize, gridSize];
+
+            //todo - if increase grid size beyond 4, add rows and columns to gridLayoutPanel
+
+            for(int r=0; r< gridSize;r++)
+            {
+                for (int c = 0; r < gridSize; r++)
+                {
+                    TextBox newTextBox = new TextBox();
+                    letterControls[r, c] = newTextBox;
+                    letters[r, c] = ' ';
+                    lettersGrid.Controls.Add(newTextBox, c, r);
+                    newTextBox.Dock = DockStyle.Fill;
+                    newTextBox.MaxLength = 1;
+                }
+            }
+        }
+        private void readLetters()
+        {
+            for (int r = 0; r < gridSize; r++)
+            {
+                for (int c = 0; r < gridSize; r++)
+                {
+                    if (!string.IsNullOrWhiteSpace(letterControls[r, c].Text))
+                    { 
+                        letters[r, c] = letterControls[r, c].Text.ToCharArray()[0];
+                    }
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
